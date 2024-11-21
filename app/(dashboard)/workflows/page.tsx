f2a66c6c30bbef665dcbe6b04b977b1030b3ed59
Ayyan Ahmed <ayyanahmed@outlook.com>
2024-11-21 16:41:37 +0500

    Workflow UI completed together with the action scripts

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -1,8 +1,13 @@
-import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
-import { Skeleton } from "@/components/ui/skeleton";
 import React, { Suspense } from "react";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertCircle, InboxIcon } from "lucide-react";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { GetWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
+
+import CreateWorkflowDialog from "@/app/(dashboard)/workflows/_components/CreateWorkflowDialog";
+import WorkflowCard from "@/app/(dashboard)/workflows/_components/WorkflowCard";
 
 function page() {
   return (
@@ -12,6 +17,7 @@ function page() {
           <h1 className="text-3xl font-bold">Workflows</h1>
           <p className="text-muted-foreground">Manage your workflows</p>
         </div>
+        <CreateWorkflowDialog />
       </div>
 
       <div className="h-full py-6">
@@ -60,11 +66,18 @@ async function UserWorkflows() {
             Click the button below to create your first workflow
           </p>
         </div>
+        <CreateWorkflowDialog triggerText="Create your first workflow" />
       </div>
     );
   }
 
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {workflows.map((workflow) => (
+        <WorkflowCard key={workflow.id} workflow={workflow} />
+      ))}
+    </div>
+  );
 }
 
 export default page;
